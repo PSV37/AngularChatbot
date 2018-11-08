a54c5d4df55b575e0ba2b0d491a3724c689a7fbb
sapna <sapnapastapure@gmail.com>
2018-11-08 21:54:07 +0530

    initial commit

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
-import { ApiAiClient } from 'api-ai-javascript';
+import { ApiAiClient } from 'api-ai-javascript/es6/ApiAiClient';
 
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
@@ -13,7 +13,7 @@ export class Message {
 @Injectable()
 export class ChatService {
 
-  readonly token = environment.dialogflow.angularBot;
+  readonly token = environment.dialogflow.AngularBot;
   readonly client = new ApiAiClient({ accessToken: this.token });
 
   conversation = new BehaviorSubject<Message[]>([]);
